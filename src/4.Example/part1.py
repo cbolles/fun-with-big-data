e8cda0dac26800d88d17876de055a60c66689388
Collin Bolles <collinbolles@gmail.com>
2019-05-04 23:38:14 -0400

    Add more advanced examples

---
+++ src/4.Example/part1.py
@@ -0,0 +1,79 @@
+"""
+This example will go over how we could use a REST API. Lets say we want to find the average price for the first 25
+virtual currencies using the REST API that was used in the previous examples. We will start by calling the REST API,
+getting the JSON, looping through the data, and calculating the average. Below is what the JSON looks like from the
+documentation
+https://www.coinlore.com/cryptocurrency-data-api
+
+{
+  "data": [
+    {
+      "id": "90",
+      "symbol": "BTC",
+      "name": "Bitcoin",
+      "nameid": "bitcoin",
+      "rank": 1,
+      "price_usd": "6456.52",
+      "percent_change_24h": "-1.47",
+      "percent_change_1h": "0.05",
+      "percent_change_7d": "-1.07",
+      "price_btc": "1.00",
+      "market_cap_usd": "111586042785.56",
+      "volume24": 3997655362.9586277,
+      "volume24a": 3657294860.710187,
+      "csupply": "17282687.00",
+      "tsupply": "17282687",
+      "msupply": "21000000"
+    },
+  "info": {
+    "coins_num": 1969,
+    "time": 1538560355
+  }
+}
+
+So we have a value "data" which is a list that contains some nested json objects (dictionaries). Inside this object
+there is a "price_usd" key with a string value of its worth. If we loop through the "data" array and convert the value
+associated with "price_usd" we can get the price of each virtual currency.
+"""
+import requests
+
+"""
+Step 1 
+First we need to get the response from the REST API. We needs to make parameters that will limit the output to be just
+25 virtual currencies. Then we get the data back by calling .json() on the get result
+"""
+get_parameters = {'limit': 25}  # The documentation shows how we can request for a limited data as response
+response_data = requests.get('https://api.coinlore.com/api/tickers/', get_parameters).json()
+
+"""
+Step 2
+We need to sift out the data we care about. Im going to make an array that will hold all the information we care about.
+In it will be the names and the prices of the currency in the format
+[ [name, price], [name, price], [name, price] ]
+"""
+useful_data = []
+for coin in response_data['data']:
+    currency_name = coin['name']
+    currency_value = float(coin['price_usd'])
+    useful_data.append([currency_name, currency_value])
+
+"""
+Step 2.5
+Lets look at what the data we just collected looks like.
+"""
+print('\nStep 2.5')
+for coin in useful_data:
+    print(coin[0], coin[1])  # Name is the first value in the array, price is the second value
+
+"""
+Step 3
+Calculating the average is straight forward from here, you can get fancy with python wizardry, but lets keep it simple
+for now
+"""
+total = 0.0
+for coin in useful_data:
+    total += coin[1]
+average = total / len(useful_data)
+print('\nThe average is', average)
+
+
---
+++ src/4.Example/part2.py
@@ -0,0 +1,119 @@
+"""
+This example will be a little more complicated. This is just to highlight a little more the potential of REST APIs
+"""
+import argparse
+import requests
+import matplotlib.pyplot as plt
+from matplotlib.ticker import FuncFormatter
+import numpy as np
+
+API_BASE_URL = 'https://api.coinlore.com/api/'
+API_ALL_COIN_EXTENSION = 'tickers/'
+API_COIN_MARKET_EXTENSION = 'coin/markets/'
+
+
+class Coin:
+    """
+    Represents the information needed to represent the volume of a virtual coin in USD.
+    args
+        :param coin_id: The id related to the coin from the REST API
+        :param name: The vernacular name of the coin
+        :param volume_usd: The total amount of the coin in USD
+    """
+    __slots__ = ['coin_id', 'name', 'volume_usd']
+
+    def __init__(self, coin_id, name, volume_usd):
+        self.coin_id = coin_id
+        self.name = name
+        self.volume_usd = volume_usd
+
+
+def get_coins(max_coins=25) ->list:
+    """
+    Get the coins from the REST API up to an including the max number passes in.
+    not yet have the volume in usd set
+    :param max_coins: The max number of coins to get information on
+    :return: A list of Coin objects with the id, name, and volume in usd included
+    """
+    get_parameter = {'limit': max_coins}
+    response_data = requests.get(API_BASE_URL + API_ALL_COIN_EXTENSION, get_parameter).json()
+    coins = []
+    for value in response_data['data']:
+        coin_id = value['id']
+        coin_name = value['name']
+        coin_volume = float(value['market_cap_usd'])
+        coins.append(Coin(coin_id, coin_name, coin_volume))
+    return coins
+
+
+def display_stats(coins: list) -> None:
+    """
+    Display some basic statistics on the coins to stdout. Show the average, max, min
+    :param coins: List of coin objects to display stats
+    :return: None
+    """
+    average_value = sum(coin.volume_usd for coin in coins) / len(coins)
+    sorted_data = sorted(coins, key=lambda coin: coin.volume_usd)
+    max_coin = sorted_data[-1]
+    min_coin = sorted_data[0]
+    print('Stats')
+    print('=====')
+    print('Total Number of Coins:', len(coins))
+    print('Average volume in USD:', '${:,}'.format(average_value))
+    print('Max Coin:', max_coin.name, 'Volume:', '${:,}'.format(max_coin.volume_usd))
+    print('Min Coin:', min_coin.name, 'Volume:', '${:,}'.format(min_coin.volume_usd))
+
+
+def show_pie_chart(coins: list) -> None:
+    """
+    Show a pie chart based on the total volume of the coin in USD. Based on the percentage of the volume of the given
+    coin compared to total volume in USD
+    :param coins: A list of coin objects to turn into a pie chart
+    :return: None
+    """
+    labels = [coin.name for coin in coins]
+    total_volume = sum([coin.volume_usd for coin in coins])
+    sizes = [coin.volume_usd / total_volume for coin in coins]
+
+    fig1, ax1 = plt.subplots()
+    ax1.pie(sizes, labels=labels, autopct='%1.1f%%')
+    ax1.axis('equal')
+    plt.show()
+
+
+def show_bar_graph(coins: list) -> None:
+    """
+    Show a bar graph based on the volume in USD for each coin. Each coin is represented by a bar graph
+    :param coins: A list of coins to graph
+    :return: None
+    """
+    formatter = FuncFormatter(lambda val, pos: '$%1.1fM' % (val * 1e-6))
+
+    x = [i for i in range(0, len(coins))]
+    volume = [coin.volume_usd for coin in coins]
+
+    fig, ax = plt.subplots()
+    ax.yaxis.set_major_formatter(formatter)
+    plt.bar(x, volume)
+    plt.xticks(x, [coin.name for coin in coins], rotation='vertical')
+    plt.show()
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Get information on the volume of different cryptocurrency')
+    parser.add_argument('max_num', action='store', type=int, help='Number of cryptocurrencies to analyse')
+    parser.add_argument('-b', '--bar', action='store_true', help='Flag to display bar graph')
+    parser.add_argument('-p', '--pie', action='store_true', help='Flag to display pie graph')
+    args = parser.parse_args()
+
+    coins = get_coins(args.max_num)
+    display_stats(coins)
+
+    if args.pie:
+        show_pie_chart(coins)
+    if args.bar:
+        show_bar_graph(coins)
+
+
+if __name__ == '__main__':
+    main()
